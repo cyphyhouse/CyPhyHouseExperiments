f25ce4db3e06dda4c4eb368a72ad46ba822c8876
Hsieh, Chiao <chsieh16@illinois.edu>
2019-07-26 18:35:58 -0500

    Refactor Charlie's start.py and call from main

--- cph/__main__.py
+++ cph/__main__.py
@@ -1,21 +1,40 @@
 """Main entry point for deployment or simulation"""
 
 from .konfig import Konfig
+from . import deploy_irl
 
 
 def main(argv) -> None:
     # TODO Parse arguments better
-    config_filename = argv[1]
-    device_name = argv[2]
-    out_filename = argv[3]
+    task_krd = argv[1]
+    global_cfg_filename = argv[2]
+    local_cfg_filename = 'local.yml'
 
-    with open(config_filename, 'r') as f:
+    # TODO Call Koord Compiler
+    task_py = task_krd
+
+    with open(global_cfg_filename, 'r') as f:
         conf = Konfig(f)
-    print(conf.gen_all_local_configs())
-    with open(out_filename, 'w', encoding='utf8') as out_file:
-        conf.dump_local_config(device_name, out_file)
 
     # TODO Run simulation or deployment scripts
+    if True:
+        device_list = deploy_irl.get_device_list()
+
+        # TODO Use device list to generate/update global config file
+
+        for device_info in device_list:
+            with open(local_cfg_filename, 'w', encoding='utf8') as out_file:
+                conf.dump_local_config(device_info.name, out_file)
+
+            # TODO Put task.py and local config to right places
+
+            deploy_irl.upload_and_exec(
+                device_addr=device_info.addr,
+                username='pi',   # TODO: read username and password from somewhere
+                password='cyphyhouse',
+                local_path="./flydrone.bash",
+                remote_path="./flydrone.bash",
+                command="bash ./flydrone.bash")
 
 
 if __name__ == "__main__":
---
+++ cph/deploy_irl.py
@@ -0,0 +1,75 @@
+"""Utilility functions for deploying in Intelligent Robot Lab environment"""
+
+from ipaddress import IPv4Address
+import paramiko
+import socket
+from typing import List, NamedTuple
+
+
+DeviceInfo = NamedTuple(
+    'DeviceInfo',
+    [
+        ('name', str),
+        ('addr', IPv4Address),
+        ('status', str)
+    ]
+)
+
+
+def get_device_list() -> List[DeviceInfo]:
+    """
+    The function broadcasts to LAN and wait for responses from devices to compile a list.
+    :return: The list of discovered devices
+    """
+    buffer_size = 1024
+    client_port = 60651
+    sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sender.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sender.sendto(b'INFO', ('<broadcast>', client_port))
+    print("[INFO] Device query sent!")  # TODO logging instead of printing
+
+    sender.settimeout(3.0)  # Set timeout to avoid waiting forever
+    device_list = []
+    while True:
+        try:
+            info, address = sender.recvfrom(buffer_size)
+            info = info.decode("utf-8").split(' ')
+            device_info = DeviceInfo(name=info[0], addr=address[0], status=info[1])
+            device_list.append(device_info)
+            print(info[0], " from ", address)
+        except socket.timeout:
+            break
+    print("[INFO] Discover finished")  # TODO logging instead of printing
+    return device_list
+
+
+def upload_and_exec(device_addr: IPv4Address,
+                    local_path: str,  # TODO use Path instead of str?
+                    remote_path: str,
+                    command: str = None,
+                    username: str = None,
+                    password: str = None,
+                    ) -> None:
+    """
+    Upload a file and execute the file as a bash script thru SSH
+    :param device_addr: IP address to upload to
+    :param local_path: The local path of file to upload
+    :param remote_path: The remote **path of file** after upload
+    :param command: command to execute after the file is uploaded
+    :param username: User name for login
+    :param password: Password for login
+    :return:
+    """
+    ssh_client = paramiko.SSHClient()
+    ssh_client.connect(hostname=str(device_addr),
+                       username=username, password=password)
+
+    print("[INFO] Upload file to: ", device_addr)  # TODO logging instead of printing
+    sftp_client = ssh_client.open_sftp()
+    sftp_client.put(local_path, remote_path)
+    sftp_client.close()
+
+    print("[INFO] Sending command \"" + command + "\" to ", device_addr)  # TODO logging instead of printing
+    stdin, stdout, stderr = ssh_client.exec_command(command)
+
+    ssh_client.close()
