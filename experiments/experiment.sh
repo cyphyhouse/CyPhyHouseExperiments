a06fd2ea5885eca6c9aea8493d2952a29baad41d
Hsieh, Chiao <chsieh16@illinois.edu>
2020-01-13 22:52:07 -0600

    Add experiment script, addnum, and example configs

---
+++ experiments/experiment.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PATH_KOORD_JAR="koord-1.0-SNAPSHOT-jar-with-dependencies.jar"
+PATH_APP_KRD=$1
+PATH_CONFIG=$2
+PATH_APP_PY=$(mktemp)
+
+TIMEOUT="120s"
+
+
+if [ ! -f "$PATH_KOORD_JAR" ]; then
+    echo "Cannot find JAR file $PATH_KOORD_JAR for compiling Koord programs"
+    exit
+fi
+
+if [ ! -f "$PATH_APP_KRD" ]; then
+    echo "Please specify a Koord program"
+    exit
+fi
+
+if [ ! -f "$PATH_CONFIG" ]; then
+    echo "Please specify a configuration file"
+    exit
+fi
+
+# TODO Start up Cymulator
+
+# Compile the given Koord program to Python
+java -jar $PATH_KOORD_JAR $PATH_APP_KRD $PATH_APP_PY
+
+# TODO Start up background processes for collecting statistics
+
+
+function finish
+{
+    # TODO kill background processes
+    echo "Terminating background processes"
+}
+trap finish EXIT
+
+# Execute Koord application with TIMEOUT
+PYTHONPATH="$PYTHONPATH:.."  # Set temporary PYTHONPATH to import cph
+timeout --foreground -s INT $TIMEOUT python3 -m cph $PATH_APP_PY $PATH_CONFIG
+
