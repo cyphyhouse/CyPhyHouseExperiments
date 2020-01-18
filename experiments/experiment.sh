a5a2631f665d200d7997bf86d1d6a7851fe69688
Hsieh, Chiao <chsieh16@illinois.edu>
2020-01-17 19:00:57 -0600

    Update script with latest Koord v0.1 and ignore JAR file

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PATH_KOORD_JAR="koord-1.0-SNAPSHOT-jar-with-dependencies.jar"
+PATH_KOORD_JAR="koord-0.1-jar-with-dependencies.jar"
 PATH_APP_KRD=$1
 PATH_CONFIG=$2
 PATH_APP_PY=$(mktemp)
@@ -10,6 +10,8 @@ TIMEOUT="120s"
 
 if [ ! -f "$PATH_KOORD_JAR" ]; then
     echo "Cannot find JAR file $PATH_KOORD_JAR for compiling Koord programs"
+    URL_KOORD="https://github.com/cyphyhouse/KoordLanguage/releases"
+    echo "You can find available Koord releases at ${URL_KOORD}"
     exit
 fi
 
