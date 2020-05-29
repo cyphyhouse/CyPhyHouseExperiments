817172c48c2cff8b098ef96dc04c841bcc26a8fa
Hsieh, Chiao <chsieh16@illinois.edu>
2020-05-28 19:01:42 -0500

    Modify follow_points and add generated Python code

---
+++ experiments/app_py/follow_points.krd.py
@@ -0,0 +1,33 @@
+from src.harness.agentThread import AgentThread
+
+
+class Follow_points(AgentThread):
+
+    def __init__(self, config, motion_config):
+        super(Follow_points, self).__init__(config, motion_config)
+
+    def initialize_vars(self):
+        self.locals = {}
+        self.locals['tries'] = 1
+        self.locals['p'] = 10 - 3 * self.pid()
+
+    def loop_body(self):
+        if self.locals['tries'] == 1:
+            self.write_to_actuator('Motion.target', self.pos3d( -self.locals['p'], self.locals['p'], 1.0))
+            self.locals['tries'] = 2
+            return
+        if self.locals['tries'] == 2 and self.read_from_sensor('Motion.reached'):
+            self.write_to_actuator('Motion.target', self.pos3d(self.locals['p'], self.locals['p'], 1.0))
+            self.locals['tries'] = 3
+            return
+        if self.locals['tries'] == 3 and self.read_from_sensor('Motion.reached'):
+            self.write_to_actuator('Motion.target', self.pos3d(self.locals['p'],  -self.locals['p'], 1.0))
+            self.locals['tries'] = 4
+            return
+        if self.locals['tries'] == 4 and self.read_from_sensor('Motion.reached'):
+            self.write_to_actuator('Motion.target', self.pos3d( -self.locals['p'],  -self.locals['p'], 1.0))
+            self.locals['tries'] = 5
+            return
+        if self.locals['tries'] == 5 and self.read_from_sensor('Motion.reached'):
+            self.trystop()
+            return
