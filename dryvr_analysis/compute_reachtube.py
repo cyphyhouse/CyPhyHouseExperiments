bd665b27bd91e0dcfa74d4c27d4e82fcf80b58c1
Hsieh, Chiao <chsieh16@illinois.edu>
2020-11-23 11:35:18 -0600

    Add some sample code computing stats about the traces

--- dryvr_analysis/compute_reachtube.py
+++ dryvr_analysis/compute_reachtube.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python3
+
 import pickle
 from typing import Any
+
+import numpy as np
 
 
 def main(argv: Any) -> None:
     for pickle_file_io in argv.pickle_file:
         mode_trace_seq = pickle.load(pickle_file_io)
+        # TODO compute reachtube from traces
         for mode, trace in mode_trace_seq:
             print("="*80)
-            print("Waypoint: %s" % mode)
-            print("Trace: %s" % trace)
+            print("Stamped Waypoint: %s" % mode)
+            print("Trace Length: %d" % len(trace))
+            avg = np.sum(trace[1:, 0] - trace[:-1, 0]) / (len(trace) - 1)
+            print("Average State Sampling Period: %f microseconds" % avg)
 
 
 if __name__ == "__main__":
