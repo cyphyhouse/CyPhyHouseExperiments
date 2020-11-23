95c205b447464a42c5a3ff34dc6a6158dcdf46c0
Hsieh, Chiao <chsieh16@illinois.edu>
2020-11-23 11:19:57 -0600

    Replace bag and pickle files to match the option values in collect_rosbags_trace.sh

--- dryvr_analysis/collect_rosbag_trace.sh
+++ dryvr_analysis/collect_rosbag_trace.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 DEVEL_DIR=$HOME/catkin_ws/devel
-BAG_DIR=$HOME/cph_ws/CyPhyHouseExperiments/experiments_utm
+BAG_DIR=/tmp/
 
-source ${DEVEL_DIR}/setup.bash
+source "${DEVEL_DIR}/setup.bash"
 
 
 # Launch ROS nodes Gazebo simulator in background
-roslaunch cym_device test_hector_quad_random_waypoints.launch bag_dir:=${BAG_DIR} num_waypoints:=500&
+roslaunch cym_device test_hector_quad_random_waypoints.launch bag_dir:=${BAG_DIR} num_waypoints:=5 time_itvl_min:="4.5"&
 ROSLAUNCH_PID=$!
 
 # Wait for all nodes finish initialization
