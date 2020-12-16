b1a610ef22eb786a82d0fef5206ff8ae7d402235
Hsieh, Chiao <chsieh16@illinois.edu>
2020-12-16 12:04:32 -0600

    Set distance range for collecting Hector Quad traces

--- dryvr_analysis/collect_rosbag_trace.sh
+++ dryvr_analysis/collect_rosbag_trace.sh
@@ -7,7 +7,8 @@ source "${DEVEL_DIR}/setup.bash"
 
 
 # Launch ROS nodes Gazebo simulator in background
-roslaunch cym_device test_hector_quad_random_waypoints.launch bag_dir:=${BAG_DIR} num_waypoints:=5 time_itvl_min:="4.5"&
+roslaunch cym_device test_hector_quad_random_waypoints.launch bag_dir:=${BAG_DIR} num_waypoints:=100 \
+    time_itvl_min:="5.5" dist_min:="4.9" dist_max:="5.1"&
 ROSLAUNCH_PID=$!
 
 # Wait for all nodes finish initialization
